be2518db8530088cb7cc9e849297a60532bc500a
matias-user <you@example.com>
2021-04-11 11:08:29 -0400

    Initial commit

---
+++ Gatos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp3
+{
+    public class Gatos
+    {
+        public string color, tipo_pelo, color_ojos;
+        public static int contador;
+        public Gatos(string color, string tpo_pelo, string color_ojos)
+        {
+            this.color = color;
+            this.tipo_pelo = tpo_pelo;
+            this.color_ojos = color_ojos;
+            ++contador;
+        }
+       
+        public void Comer()
+        {
+            Console.WriteLine("Gato comiendo...");
+        }
+        public void Maullar()
+        {
+            Console.WriteLine("Gato maullando...");
+        }
+        public void Dormir()
+        {
+            Console.WriteLine("Gato Durmiendo...");
+        }
+    }
+    public class Siames : Gatos
+    {
+        public Siames(string color, string tpo_pelo, string color_ojos) : base(color, tpo_pelo, color_ojos)
+        {
+            this.color = color;
+            this.tipo_pelo = tpo_pelo;
+            this.color_ojos = color_ojos;
+        }
+    }
+    public class Maine : Gatos
+    {
+        public Maine(string color, string tpo_pelo, string color_ojos) : base(color, tpo_pelo, color_ojos)
+        {
+            this.color = color;
+            this.tipo_pelo = tpo_pelo;
+            this.color_ojos = color_ojos;
+        }
+    }
+    public class Persa : Gatos
+    {
+        public Persa(string color, string tpo_pelo, string color_ojos) : base(color, tpo_pelo, color_ojos)
+        {
+            this.color = color;
+            this.tipo_pelo = tpo_pelo;
+            this.color_ojos = color_ojos;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Siames gatoS = new Siames("Blanco","bkn","Verdes" );
+            Persa gatoP = new Persa("Negro", "Nice", "cafes");
+
+            Console.WriteLine(Gatos.contador);
+            Console.ReadKey();
+        }
+        //public class A
+        //{
+        //    public virtual void Hola()
+        //    {
+        //        Console.WriteLine("Hola soy A");
+        //    }
+        //}
+        //public class B : A
+        //{
+        //    public sealed override void Hola()
+        //    {
+        //        Console.WriteLine("Hola soy B");
+        //        base.Hola();
+        //    }
+        //}
+        //public class C : B
+        //{
+        //    public new void Hola()
+        //    {
+        //        Console.WriteLine("Hola soy C");
+        //        base.Hola();
+        //    }
+        }
+    }
